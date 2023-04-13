4450eb0e445adabb648a3b947dde302e3777a9f5
d3ter <tholowy@gmail.com>
2023-04-13 00:09:35 -0500

    [fix] init proyect load

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from './Header.svelte';
+	import './styles.css';
+	import "../app.css";
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+	
+	</footer>
+</div>
